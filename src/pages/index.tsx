ae1ca67e1886cc2c4d968f52cdd41122668b7b23
Thainá Freitas <thaina.augustafs@outlook.com>
2022-12-12 17:59:26 -0300

    correção yarn build

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,12 +9,12 @@ export default function Home(props) {
 
 export async function getStaticProps() {
   const response = await fetch('http://localhost:3333/episodes')
-  const data = response.json()
+  const data = await response.json()
 
   return {
     props: {
       episodes: data,
     },
-    revalidate: 60 * 60 * 8,
+    revalidate: 60 * 60 * 8,  
   }
 }
