dae3dee44e65b66656549d52e36cae490269c185
Thainá Freitas <thaina.augustafs@outlook.com>
2022-12-14 13:48:45 -0300

    adição axios

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,18 @@
 import { GetStaticProps } from "next";
+import { api } from "../services/api";
 
-export default function Home(props) {
+type Episode = {
+  id: string;
+  title: string;
+  members: string;
+  // ...
+}
+
+type HomeProps = {
+  episodes: Array<Episode>
+}
+
+export default function Home(props: HomeProps) {
   return (
     <div>
       <h1>Index</h1>
@@ -9,14 +21,20 @@ export default function Home(props) {
   )
 }
 
-export async function getStaticProps() {
-  const response = await fetch('http://localhost:3333/episodes')
-  const data = await response.json()
+export const getStaticProps: GetStaticProps = async () => {
+  const { data } = await api.get('episodes', {
+    params: {
+      _limit: 12,
+      _sort: 'published_at',
+      _order: 'desc'
+    }
+  })
+  
 
   return {
     props: {
       episodes: data,
     },
-    revalidate: 60 * 60 * 8,  
+    revalidate: 60 * 60 * 8,
   }
 }
