bab9453a1ec0534510a9202ed1fe48dc411bbc95
Thainá Freitas <thaina.augustafs@outlook.com>
2022-12-14 21:00:11 -0300

    correção listagem - aula 3 / 55min

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -63,12 +63,14 @@ export default function Home({ latestEpisodes, allEpisodes }: HomeProps) {
 
         <table cellSpacing={0}>
           <thead>
+            <tr>
             <th></th>
             <th>Podcast</th>
             <th>Integrantes</th>
             <th>Data</th>
             <th>Duração</th>
             <th></th>
+            </tr>
           </thead>
           <tbody>
             {allEpisodes.map(episode => {
