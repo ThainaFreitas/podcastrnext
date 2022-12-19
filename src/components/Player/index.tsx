2be714ff4ef330c90dc9b7a1c24ce10805119ac6
Thainá Freitas <thaina.augustafs@outlook.com>
2022-12-19 03:01:33 -0300

    formatação button - final aula 4

--- src/components/Player/index.tsx
+++ src/components/Player/index.tsx
@@ -1,8 +1,10 @@
 import { useContext } from 'react';
+import Image from 'next/image';
+import Slider from 'rc-slider';
+import 'rc-slider/assets/index.css';
 
 import { PlayerContext } from '../../contexts/PlayerContext';
 
-import Image from 'next/image';
 
 import styles from './styles.module.scss';
 
@@ -17,49 +19,57 @@ export function Player() {
                 <img src='/playing.svg' alt='Tocando agora' />
                 <strong>Tocando agora </strong>
             </header>
-        { episode ? (
-            <div className={styles.currentEpisode}>
-                <Image 
-                width={300} 
-                height={300} 
-                src={episode.thumbnail} 
-                alt={episode.title} 
-                objectFit='cover'
-                />
-                <strong>{episode.title}</strong>
-                <span>{episode.members}</span>
+            {episode ? (
+                <div className={styles.currentEpisode}>
+                    <Image
+                        width={300}
+                        height={300}
+                        src={episode.thumbnail}
+                        alt={episode.title}
+                        objectFit='cover'
+                    />
+                    <strong>{episode.title}</strong>
+                    <span>{episode.members}</span>
 
-            </div>
-        ) : (
+                </div>
+            ) : (
 
-            <div className={styles.emptyPlayer}>
-                <strong>Selecione um podcast para ouvir</strong>
-            </div>
-        )}
-         
+                <div className={styles.emptyPlayer}>
+                    <strong>Selecione um podcast para ouvir</strong>
+                </div>
+            )}
 
-            <footer className={styles.empty}>
+
+            <footer className={!episode ? styles.empty : ''}>
                 <div className={styles.progress}>
                     <span>00:00</span>
                     <div className={styles.slider}>
-                        <div className={styles.emptySlider} />
+                        { episode ? (
+                            <Slider 
+                            trackStyle={{ backgroundColor: '#04d361' }}
+                            railStyle={{backgroundColor: '#9f75ff' }}
+                            handleStyle={{borderColor: '#04d361', borderWidth: 4 }}
+                            />
+                        ) : (
+                            <div className = {styles.emptySlider} />
+                    )}
                     </div>
                     <span>00:00</span>
                 </div>
                 <div className={styles.buttons}>
-                    <button type='button'>
+                    <button type='button' disabled={!episode}>
                         <img src='/shuffle.svg' alt='Embaralhar' />
                     </button>
-                    <button type='button'>
+                    <button type='button' disabled={!episode}>
                         <img src='/play-previous.svg' alt='Tocar anterior' />
                     </button>
-                    <button type='button' className={styles.playButton}>
+                    <button type='button' className={styles.playButton} disabled={!episode}>
                         <img src='/play.svg' alt='Tocar' />
                     </button>
-                    <button type='button'>
+                    <button type='button' disabled={!episode}>
                         <img src='/play-next.svg' alt='Tocar próxima' />
                     </button>
-                    <button type='button'>
+                    <button type='button' disabled={!episode}>
                         <img src='/repeat.svg' alt='Repetir' />
                     </button>
                 </div>
